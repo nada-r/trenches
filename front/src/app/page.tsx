bd0427c9366f49b4feedccd201b22e841c1f27c4
nada-r <nadarzer@gmail.com>
2024-09-01 11:14:26 +0200

    test env

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -7,40 +7,25 @@ import '@/lib/env';
 import ArrowLink from '@/components/links/ArrowLink';
 import ButtonLink from '@/components/links/ButtonLink';
 import UnderlineLink from '@/components/links/UnderlineLink';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { Card, Power } from '@prisma/client';
+
 
 import Logo from '~/svg/Logo.svg';
 import DisplayCard from '@/components/DisplayCard';
 
 export default function HomePage() {
 
-  const [cards, setCards] = useState<(Card & { power: Power })[]>([]);
-
-  useEffect(() => {
-    async function fetchCards() {
-      try {
-        const response = await axios.get("http://localhost:3000/cards");
-        setCards(response.data);
-      } catch (error) {
-        console.error("Error fetching cards:", error);
-      }
-    }
-    fetchCards();
-  }, []);
-
+  
+  console.log(process.env)
   return (
     <main>
       <Head>
         <title>Hi</title>
       </Head>
-      <>{JSON.stringify(cards)}</>
-      {cards.length > 0 && <DisplayCard card={cards[0]} />}
       <section className='bg-white'>
         <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
           <Logo className='w-16' />
           <h1 className='mt-4'>Welcome to trenches.top</h1>
+          
           <p className='mt-2 text-sm text-gray-800'>
             Find alpha, discover the best callers, make it out of the trenches{' '}
           </p>
