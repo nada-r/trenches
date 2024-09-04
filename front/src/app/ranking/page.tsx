7f9bb11ef504b8dbe14ea049f71fe6c787f8276a
nada-r <nadarzer@gmail.com>
2024-09-04 13:03:49 +0200

    add route db

--- front/src/app/ranking/page.tsx
+++ front/src/app/ranking/page.tsx
@@ -25,7 +25,7 @@ export default function Ranking() {
   useEffect(() => {
     async function fetchCards() {
       try {
-        const response = await axios.get(process.env.NEXT_PUBLIC_BASE_URL!);
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_BASE_URL!}/cards`);
         setCards(response.data);
       } catch (error) {
         console.error("Error fetching cards:", error);
