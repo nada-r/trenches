d83b23a4fa7e8973b05cf6cf9e567eecd6667543
nico <nicolascoquelet@gmail.com>
2024-09-10 21:01:00 +0200

    extract SimCardIcon component

--- front/src/components/icons/SimCardIcon.tsx
+++ front/src/components/icons/SimCardIcon.tsx
@@ -1,36 +1,26 @@
+// SimCardIcon.tsx
 import React from 'react';
+import { FaSimCard } from 'react-icons/fa6';
 
-const styles = {
-  Icon: {
-    color: '#cd7f32',
-    fill: '#cd7f32',
-    fontSize: '46px',
-    top: '159px',
-    left: '26px',
-    width: '46px',
-    height: '86px',
-  },
+const getColorFromType = (type: string): string => {
+  switch (type.toLowerCase()) {
+    case 'bronze':
+      return '#CD7F32';
+    case 'silver':
+      return '#C0C0C0';
+    case 'gold':
+      return '#FFD700';
+    default:
+      return '#000000'; // Default color if type doesn't match
+  }
 };
 
-const SimCardIconComponent = () => (
-  <svg style={styles.Icon} viewBox="0 0 24 24">
-    <path d="M0 0h24v24H0z" fill="none"></path>
-    <path d="M19.99 4c0-1.1-.89-2-1.99-2h-8L4 8v12c0 1.1.9 2 2 2h12.01c1.1 0 1.99-.9 1.99-2l-.01-16zM9 19H7v-2h2v2zm8 0h-2v-2h2v2zm-8-4H7v-4h2v4zm4 4h-2v-4h2v4zm0-6h-2v-2h2v2zm4 2h-2v-4h2v4z"></path>
-  </svg>
-);
-
-const defaultProps = {
-  SimCardIconComponent,
-};
-
-const SimCardIcon = (props: {
-  IconComponent?: React.ComponentType<{ style: React.CSSProperties }>;
-}) => {
-  return props.IconComponent ? (
-    <props.IconComponent style={styles.Icon} />
-  ) : (
-    <defaultProps.SimCardIconComponent />
-  );
+interface SimCardIconProps {
+  type: string;
+  size?: number;
+}
+const SimCardIcon = ({ type, size = 48 }: SimCardIconProps) => {
+  return <FaSimCard color={getColorFromType(type)} size={size} />;
 };
 
 export default SimCardIcon;
--- front/src/components/ui/CallerCards.tsx
+++ front/src/components/ui/CallerCards.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
 import RankingImage from '@/components/ui/RankingImage';
-import { FaSimCard } from 'react-icons/fa6';
+import SimCardIcon from '@/components/icons/SimCardIcon';
 
 const Card = ({ type }: { type: string }) => {
-  const getColorFromType = (type: string): string => {
-    switch (type.toLowerCase()) {
-      case 'bronze':
-        return '#CD7F32';
-      case 'silver':
-        return '#C0C0C0';
-      case 'gold':
-        return '#FFD700';
-      default:
-        return '#000000'; // Default color if type doesn't match
-    }
-  };
-
   return (
     <div className="flex flex-col items-center p-4">
-      <FaSimCard color={getColorFromType(type)} size={48} />
+      <SimCardIcon type={type} size={48} />
       <div className="my-2">{type}</div>
       <RankingImage
         image={'https://trenches.fra1.cdn.digitaloceanspaces.com/Zorrogems.jpg'}
