624dbeb5717b8b456ce492f357c0038bf856985b
nico <nicolascoquelet@gmail.com>
2024-09-16 09:37:59 +0200

    remove .js files

--- backend/src/services/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PowerService = exports.UserService = exports.CardService = void 0;
-var CardService_1 = require("./CardService");
-Object.defineProperty(exports, "CardService", { enumerable: true, get: function () { return CardService_1.CardService; } });
-var UserService_1 = require("./UserService");
-Object.defineProperty(exports, "UserService", { enumerable: true, get: function () { return UserService_1.UserService; } });
-var PowerService_1 = require("./PowerService");
-Object.defineProperty(exports, "PowerService", { enumerable: true, get: function () { return PowerService_1.PowerService; } });
