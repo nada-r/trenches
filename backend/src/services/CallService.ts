941f95e935bab63f0a7c036f3c0a42dde677a4de
nico <nicolascoquelet@gmail.com>
2024-09-18 22:05:04 +0200

    apply linter

--- backend/src/services/CallService.ts
+++ backend/src/services/CallService.ts
@@ -1,59 +1,55 @@
-import { PrismaClient, Call, Prisma } from '@prisma/client';
+import { Call, Prisma, PrismaClient } from '@prisma/client';
 import { OmitPrisma } from '@src/types';
 
 export class CallService {
-    constructor(private prisma: PrismaClient) {}
+  constructor(private prisma: PrismaClient) {}
 
-    async createCall(data: OmitPrisma<Call>): Promise<Call> {
-        const call = await this.prisma.call.create({
-            data: {
-                ...data,
-                // Assurez-vous que le champ `data` respecte le type `InputJsonValue`
-                data: data.data as Prisma.InputJsonValue,
-            },
-        });
-        return call;
-    }
+  async createCall(data: OmitPrisma<Call>): Promise<Call> {
+    return this.prisma.call.create({
+      data: {
+        ...data,
+        // Assurez-vous que le champ `data` respecte le type `InputJsonValue`
+        data: data.data as Prisma.InputJsonValue,
+      },
+    });
+  }
 
-    async getCallsByTelegramId(telegramId: string): Promise<Call[]> {
-        const calls = await this.prisma.call.findMany({
-            where: {
-                caller: {
-                    telegramId: telegramId,
-                },
-            },
-        });
-        return calls;
-    }
+  async getCallsByTelegramId(telegramId: string): Promise<Call[]> {
+    return this.prisma.call.findMany({
+      where: {
+        caller: {
+          telegramId: telegramId,
+        },
+      },
+    });
+  }
 
-    async getCallByTelegramIdAndToken(
-        telegramId: string,
-        tokenAddress: string
-    ): Promise<Call | null> {
-        const call = await this.prisma.call.findFirst({
-            where: {
-                tokenAddress: tokenAddress,
-                caller: {
-                    telegramId: telegramId,
-                },
-            },
-        });
-        return call;
-    }
+  async getCallByTelegramIdAndToken(
+    telegramId: string,
+    tokenAddress: string
+  ): Promise<Call | null> {
+    return this.prisma.call.findFirst({
+      where: {
+        tokenAddress: tokenAddress,
+        caller: {
+          telegramId: telegramId,
+        },
+      },
+    });
+  }
 
   async getActiveCalls(): Promise<Call[]> {
-    const activeCalls = await this.prisma.call.findMany({
+    return this.prisma.call.findMany({
       where: {
         createdAt: {
           gte: new Date(Date.now() - 24 * 60 * 60 * 1000),
         },
       },
     });
-    return activeCalls;
   }
 
   async updateHighestFdvByToken(token: string, newFDV: number): Promise<any> {
-    const result = await this.prisma.call.updateMany({
+    return this.prisma.call.updateMany({
       where: {
         tokenAddress: token,
         highestFDV: { lt: newFDV },
@@ -66,6 +62,5 @@ export class CallService {
         updatedAt: new Date(),
       },
     });
-    return result;
   }
 }
