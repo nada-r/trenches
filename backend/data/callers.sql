1e2bc00480d14f181b2bd112af9c8458db012bd6
nico <nicolascoquelet@gmail.com>
2024-09-18 10:07:53 +0200

    current caller backup

---
+++ backend/data/callers.sql
@@ -0,0 +1,10 @@
+INSERT INTO public."Caller" (name,image,"telegramId","createdAt","updatedAt") VALUES
+	 ('Luca_Apes','https://trenches.fra1.cdn.digitaloceanspaces.com/Luca%20Apes.jpg','@Luca_Apes','2024-09-17 22:41:27.303','2024-09-17 22:41:27.303'),
+	 ('SDsCASINO','https://trenches.fra1.cdn.digitaloceanspaces.com/SD.jpg','@SDsCASINO','2024-09-17 22:41:27.311','2024-09-17 22:41:27.311'),
+	 ('Zorrogems','https://trenches.fra1.cdn.digitaloceanspaces.com/Zorrogems.jpg','@Zorrogems','2024-09-17 22:41:27.313','2024-09-17 22:41:27.313'),
+	 ('NFG','https://trenches.fra1.cdn.digitaloceanspaces.com/NFG.jpg','@NFG','2024-09-17 22:41:27.313','2024-09-17 22:41:27.313'),
+	 ('luigiscalls','https://trenches.fra1.cdn.digitaloceanspaces.com/Luigicall.jpg','@luigiscalls','2024-09-17 22:41:27.314','2024-09-17 22:41:27.314'),
+	 ('greg','https://assets.api.uizard.io/api/cdn/stream/ddcabbc0-9002-4ebc-8bfc-2a47b3f636b1.png','@greg','2024-09-17 22:41:27.315','2024-09-17 22:41:27.315'),
+	 ('Ansem','https://assets.api.uizard.io/api/cdn/stream/848ade87-3e76-42a4-9842-ebacf29c9749.png','@Ansem','2024-09-17 22:41:27.316','2024-09-17 22:41:27.316'),
+	 ('bqsed16z','https://assets.api.uizard.io/api/cdn/stream/e53ae5aa-f673-43e7-b711-a691a3603a64.png','@bqsed16z','2024-09-17 22:41:27.317','2024-09-17 22:41:27.317'),
+	 ('wallstreetbets','https://assets.api.uizard.io/api/cdn/stream/5d1295d1-654a-47cd-a559-f4eea99a0f7c.png','@wallstreetbets','2024-09-17 22:41:27.318','2024-09-17 22:41:27.318');
