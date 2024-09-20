4a0cc233869131b94f6770ca1e2664c8897e4d83
nico <nicolascoquelet@gmail.com>
2024-09-20 14:58:53 +0200

    desc

--- front/src/constant/config.ts
+++ front/src/constant/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   title: 'Trenches.top',
-  description:
-    'Your crypto battlefield HQ. Dive into meme coin madness, discover alpha callers, and join the ultimate fantasy tournaments. From the trenches to the moon!',
+  description: 'Find alpha, discover the best callers, make it out of the trenches',
+  //description: 'Your crypto battlefield HQ. Dive into meme coin madness, discover alpha callers, and join the ultimate fantasy tournaments. From the trenches to the moon!',
   url: 'https://trenches.top',
 };
