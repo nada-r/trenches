9f293666df28011942c975d46d52c40348e361c8
nada-r <nadarzer@gmail.com>
2024-09-21 12:56:13 +0800

    pin caller images on IFPS

--- spl_token_template/mint.ts
+++ spl_token_template/mint.ts
@@ -3,41 +3,49 @@ import { TokenStandard, createAndMint, mplTokenMetadata } from '@metaplex-founda
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
 const secret = require('../backend/dev-wallet.json');
 import bs58 from "bs58";
+import { pinata } from './utils/config'
 
-const umi = createUmi('https://divine-necessary-scion.solana-devnet.quiknode.pro/c6f57b9e59ed38a658fa9516d87df8a8f4351ec9');
 
-const secretKey = bs58.decode(secret.skey);
+async function loadURL(){
+    const upload = await pinata.upload.url("https://trenches.fra1.cdn.digitaloceanspaces.com/profile_pictures/6255998913.jpg");
+    console.log(upload);
+}
 
-const userWallet = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secretKey));
+loadURL();
+// const umi = createUmi('https://divine-necessary-scion.solana-devnet.quiknode.pro/c6f57b9e59ed38a658fa9516d87df8a8f4351ec9');
 
-const userWalletSigner = createSignerFromKeypair(umi, userWallet);
+// const secretKey = bs58.decode(secret.skey);
 
-const mint = generateSigner(umi);
-umi.use(signerIdentity(userWalletSigner));
-umi.use(mplTokenMetadata())
+// const userWallet = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secretKey));
 
-const metadata = {
-    name: "luigiscalls",
-    symbol: "LGC",
-    uri: "https://turquoise-quickest-wasp-930.mypinata.cloud/ipfs/QmWqkf64nxiZfsLgrVBRdrp6eFAwJfSvAUYvzDnZVEkP2D",
-};
+// const userWalletSigner = createSignerFromKeypair(umi, userWallet);
 
-createAndMint(umi, {
-    mint,
-    authority: umi.identity,
-    name: metadata.name,
-    symbol: metadata.symbol,
-    uri: metadata.uri,
-    sellerFeeBasisPoints: percentAmount(0),
-    decimals: 8,
-    amount: 1000000000_00000000,
-    tokenOwner: userWallet.publicKey,
-    tokenStandard: TokenStandard.Fungible,
-}).sendAndConfirm(umi)
-    .then(() => {
-        console.log("Successfully minted 1 billion tokens (", mint.publicKey, ")");
-    })
-    .catch((err) => {
-        console.error("Error minting tokens:", err);
-    });
+// const mint = generateSigner(umi);
+// umi.use(signerIdentity(userWalletSigner));
+// umi.use(mplTokenMetadata())
+
+// const metadata = {
+//     name: "luigiscalls",
+//     symbol: "LGC",
+//     uri: "https://turquoise-quickest-wasp-930.mypinata.cloud/ipfs/QmWqkf64nxiZfsLgrVBRdrp6eFAwJfSvAUYvzDnZVEkP2D",
+// };
+
+// createAndMint(umi, {
+//     mint,
+//     authority: umi.identity,
+//     name: metadata.name,
+//     symbol: metadata.symbol,
+//     uri: metadata.uri,
+//     sellerFeeBasisPoints: percentAmount(0),
+//     decimals: 8,
+//     amount: 1000000000_00000000,
+//     tokenOwner: userWallet.publicKey,
+//     tokenStandard: TokenStandard.Fungible,
+// }).sendAndConfirm(umi)
+//     .then(() => {
+//         console.log("Successfully minted 1 billion tokens (", mint.publicKey, ")");
+//     })
+//     .catch((err) => {
+//         console.error("Error minting tokens:", err);
+//     });
 
--- spl_token_template/utils/config.ts
+++ spl_token_template/utils/config.ts
@@ -1,8 +1,11 @@
 "server only"
 
 import { PinataSDK } from "pinata-web3"
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const pinata = new PinataSDK({
-  pinataJwt: `${process.env.PINATA_JWT}`,
-  pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`
-})
+    pinataJwt: process.env.NEXT_PUBLIC_PINATA_JWT,
+    pinataGateway: process.env.NEXT_PUBLIC_GATEWAY_URL
+  })
