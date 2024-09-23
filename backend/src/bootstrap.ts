d5ecdde1a63443ba879e323475ab9bbb144005d8
nico <nicolascoquelet@gmail.com>
2024-09-23 12:18:49 +0200

    fix env

--- backend/src/bootstrap.ts
+++ backend/src/bootstrap.ts
@@ -4,8 +4,11 @@ import {
   CallerService,
   CallingPowerService,
   CallService,
+  ClaimService,
+  TournamentScoreService,
   TournamentService,
 } from './services';
+import 'dotenv/config';
 
 // Make sure all the Envs are loaded when launching the server
 // add the new env under envVariables
@@ -30,16 +33,27 @@ export default async function bootstrap() {
 
   const callerService = new CallerService(prisma);
   const callService = new CallService(prisma);
-  const tournamentService = new TournamentService(prisma);
   const callingPowerService = new CallingPowerService(
     callerService,
     callService
   );
+  const tournamentScoreService = new TournamentScoreService(
+    callerService,
+    callService,
+    callingPowerService,
+    prisma
+  );
+  const tournamentService = new TournamentService(
+    tournamentScoreService,
+    prisma
+  );
+  const claimService = new ClaimService(prisma);
 
   return {
     callerService,
     callService,
     callingPowerService,
+    claimService,
     tournamentService,
     prisma,
   };
