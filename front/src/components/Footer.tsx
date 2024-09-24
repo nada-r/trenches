2b6a22e74a481afb97555c81360539b2b7cbf12a
nada-r <nadarzer@gmail.com>
2024-09-24 10:32:45 +0200

    fix routes

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -8,21 +8,25 @@ import TournamentIcon from "./icons/TournamentIcon";
 const menulist=[{
   icon: <RankIcon />,
   name: "Ranking",
+  href:"/ranking",
 },{
   icon: <PortfolioIcon />,
   name: "Portfolio",
+  href:"/portfolio",
 },{
   icon: <MarketIcon />,
   name: "Market",
+  href:"/market",
 },{
   icon: <TournamentIcon />,
   name: "Tournament",
+  href:"/tournament",
 }]
 
 const Footer: React.FC = () => {
   return (
     <footer className="fixed bottom-0 w-full flex justify-around bg-background text-foreground border-t border-border py-2 items-baseline"> {/* Apply global background, text color, and border */}
-      {menulist.map(menu=> <Link href="/ranking" className="text-foreground no-underline">
+      {menulist.map(menu=> <Link href={menu.href} className="text-foreground no-underline">
         <div className="flex flex-col items-center">
           <div className="shrink-0">{menu.icon}</div>
           <p className="m-0 text-xs">{menu.name}</p>
