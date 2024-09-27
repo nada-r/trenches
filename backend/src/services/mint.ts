619402a2d2f366d410a9b8f7d2550767e75029f2
nico <nicolascoquelet@gmail.com>
2024-09-27 09:47:34 +0200

    delay the umi initialization

--- backend/src/services/mint.ts
+++ backend/src/services/mint.ts
@@ -1,55 +1,85 @@
-import { percentAmount, generateSigner, signerIdentity, createSignerFromKeypair } from '@metaplex-foundation/umi'
-import { TokenStandard, createAndMint, mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata'
+import {
+  createSignerFromKeypair,
+  generateSigner,
+  Keypair,
+  percentAmount,
+  Signer,
+  signerIdentity,
+  Umi,
+} from '@metaplex-foundation/umi';
+import {
+  createAndMint,
+  mplTokenMetadata,
+  TokenStandard,
+} from '@metaplex-foundation/mpl-token-metadata';
 import { createUmi } from '@metaplex-foundation/umi-bundle-defaults';
-import bs58 from "bs58";
-import { pinata } from '../util/pinataconfig'
+import bs58 from 'bs58';
+import { pinata } from '../util/pinataconfig';
 
-const umi = createUmi('https://divine-necessary-scion.solana-devnet.quiknode.pro/c6f57b9e59ed38a658fa9516d87df8a8f4351ec9');
+let umi: Umi;
+let userWallet: Keypair;
+let userWalletSigner: Signer;
+let mint: Signer;
 
-const secretKey = process.env.DEV_WALLET_SKEY ? bs58.decode(process.env.DEV_WALLET_SKEY) : null;
+function initializeUmi() {
+  if (umi) {
+    return { umi, userWallet, userWalletSigner, mint };
+  }
+  umi = createUmi(
+    'https://divine-necessary-scion.solana-devnet.quiknode.pro/c6f57b9e59ed38a658fa9516d87df8a8f4351ec9'
+  );
 
-if (!secretKey) {
-    throw new Error("DEV_WALLET_SKEY environment variable is not set");
+  const secretKey = process.env.DEV_WALLET_SKEY
+    ? bs58.decode(process.env.DEV_WALLET_SKEY)
+    : null;
+
+  if (!secretKey) {
+    throw new Error('DEV_WALLET_SKEY environment variable is not set');
+  }
+
+  userWallet = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secretKey));
+
+  userWalletSigner = createSignerFromKeypair(umi, userWallet);
+
+  mint = generateSigner(umi);
+  umi.use(signerIdentity(userWalletSigner));
+  umi.use(mplTokenMetadata());
+
+  return { umi, userWallet, userWalletSigner, mint };
 }
 
-const userWallet = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(secretKey));
-
-const userWalletSigner = createSignerFromKeypair(umi, userWallet);
-
-const mint = generateSigner(umi);
-umi.use(signerIdentity(userWalletSigner));
-umi.use(mplTokenMetadata());
+export { initializeUmi };
 
 async function setupMetadata(image: string, callerName: string) {
-    const upload = await pinata.upload.url(image);
+  const upload = await pinata.upload.url(image);
 
-    const metadata = {
-        name: callerName,
-        symbol: callerName.slice(0, 3).toUpperCase(),
-        uri: `https://turquoise-quickest-wasp-930.mypinata.cloud/ipfs/${upload.IpfsHash}`,
-    };
-
-    return metadata;
+  return {
+    name: callerName,
+    symbol: callerName.slice(0, 3).toUpperCase(),
+    uri: `https://turquoise-quickest-wasp-930.mypinata.cloud/ipfs/${upload.IpfsHash}`,
+  };
 }
 
 export async function mintToken(image: string, callerName: string) {
-    try {
-        const metadata = await setupMetadata(image, callerName);//"https://trenches.fra1.cdn.digitaloceanspaces.com/profile_pictures/6255998913.jpg", "luigiscall");
+  try {
+    initializeUmi();
 
-        await createAndMint(umi, {
-            mint,
-            authority: umi.identity,
-            name: metadata.name,
-            symbol: metadata.symbol,
-            uri: metadata.uri,
-            sellerFeeBasisPoints: percentAmount(0),
-            decimals: 8,
-            amount: 1000000000_00000000, // 1 billion tokens
-            tokenOwner: userWallet.publicKey,
-            tokenStandard: TokenStandard.Fungible,
-        }).sendAndConfirm(umi);
-        console.log("Successfully minted 1 billion tokens (", mint.publicKey, ")");
-    } catch (err) {
-        console.error("Error minting tokens:", err);
-    }
+    const metadata = await setupMetadata(image, callerName); //"https://trenches.fra1.cdn.digitaloceanspaces.com/profile_pictures/6255998913.jpg", "luigiscall");
+
+    await createAndMint(umi, {
+      mint,
+      authority: umi.identity,
+      name: metadata.name,
+      symbol: metadata.symbol,
+      uri: metadata.uri,
+      sellerFeeBasisPoints: percentAmount(0),
+      decimals: 8,
+      amount: 1000000000_00000000, // 1 billion tokens
+      tokenOwner: userWallet.publicKey,
+      tokenStandard: TokenStandard.Fungible,
+    }).sendAndConfirm(umi);
+    console.log('Successfully minted 1 billion tokens (', mint.publicKey, ')');
+  } catch (err) {
+    console.error('Error minting tokens:', err);
+  }
 }
