abd876ef08348c8b41374c8aebbe8c15dc482683
nico <nicolascoquelet@gmail.com>
2024-09-28 09:26:59 +0200

    remove max width in desktop

--- front/src/app/template.tsx
+++ front/src/app/template.tsx
@@ -8,7 +8,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
   const { authenticated } = usePrivy();
 
   return (
-    <div className="flex flex-col h-screen lg:max-w-2xl lg:mx-auto">
+    <div className="flex flex-col h-screen">
       {authenticated && <Header />}
 
       <main className="flex-1 p-2">{children}</main>
