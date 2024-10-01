fba6627605a2700638485a30d6243a21807f8506
nico <nicolascoquelet@gmail.com>
2024-10-01 21:43:08 +0200

    align columns names

--- front/src/app/call/page.tsx
+++ front/src/app/call/page.tsx
@@ -106,72 +106,80 @@ export default function CallPage() {
             <TableHead className="text-muted-foreground border-border text-white">
               Alpha callers
             </TableHead>
-            <TableHead className="text-muted-foreground border-border">
+            <TableHead className="text-muted-foreground border-border text-right">
               Mcap
             </TableHead>
-            <TableHead className="text-muted-foreground border-border">
+            <TableHead className="text-muted-foreground border-border text-right">
               Start
             </TableHead>
-            <TableHead className="text-muted-foreground border-border">
+            <TableHead className="text-muted-foreground border-border text-right">
               Highest
             </TableHead>
-            <TableHead className="text-muted-foreground border-border">
+            <TableHead className="text-muted-foreground border-border text-right">
               Multiple
             </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
-          {calls.map((call, index) => (
-            <TableRow key={call.id} className="border-border">
-              <TableCell
-                className={`font-medium text-foreground border-border ${call.multiple >= 2 ? 'text-green-500' : 'text-yellow-500'}`}
-              >
-                <TokenTicker
-                  ticker={call.token.ticker}
-                  address={call.token.address}
-                  showCopy
-                />
-              </TableCell>
-              <TableCell className="border-border pr-2 text-center hidden sm:block">
-                <TokenAddress address={call.tokenAddress} iconOnly />
-              </TableCell>
-              <TableCell className="text-foreground border-border pl-2">
-                <TokenLinks token={call.token} />
-              </TableCell>
-              <TableCell className="border-border pr-2 text-right">
-                <div className="flex justify-end">
-                  <CallerAvatar
-                    name={call.caller?.name || ''}
-                    image={
-                      call.caller?.image === null
-                        ? undefined
-                        : call.caller?.image
-                    }
-                    className="w-7 h-7"
+          {calls.map((call, index) => {
+            const callColor =
+              call.multiple >= 2 ? 'text-green-500' : 'text-yellow-500';
+            return (
+              <TableRow key={call.id} className="border-border">
+                <TableCell
+                  className={`font-medium text-foreground border-border ${callColor}`}
+                >
+                  <TokenTicker
+                    ticker={call.token.ticker}
+                    address={call.token.address}
+                    showCopy
                   />
-                </div>
-              </TableCell>
-              <TableCell className="text-foreground border-border">
-                {call.caller.name}
-              </TableCell>
-              <TableCell className="text-foreground border-border">-</TableCell>
-              <TableCell
-                className={`text-foreground border-border text-right ${call.multiple >= 2 ? 'text-green-500' : 'text-yellow-500'}`}
-              >
-                <FDV value={call.startFDV} light={true} />
-              </TableCell>
-              <TableCell
-                className={`text-foreground border-border text-right ${call.multiple >= 2 ? 'text-green-500' : 'text-yellow-500'}`}
-              >
-                <FDV value={call.highestFDV} light={true} />
-              </TableCell>
-              <TableCell
-                className={`text-foreground border-border text-right ${call.multiple >= 2 ? 'text-green-500' : 'text-yellow-500'}`}
-              >
-                <CallMultiple multiple={call.multiple} />
-              </TableCell>
-            </TableRow>
-          ))}
+                </TableCell>
+                <TableCell className="border-border pr-2 text-center hidden sm:block">
+                  <TokenAddress address={call.tokenAddress} iconOnly />
+                </TableCell>
+                <TableCell className="text-foreground border-border pl-2">
+                  <TokenLinks token={call.token} />
+                </TableCell>
+                <TableCell className="border-border pr-2 text-right">
+                  <div className="flex justify-end">
+                    <CallerAvatar
+                      name={call.caller?.name || ''}
+                      image={
+                        call.caller?.image === null
+                          ? undefined
+                          : call.caller?.image
+                      }
+                      className="w-7 h-7"
+                    />
+                  </div>
+                </TableCell>
+                <TableCell className="text-foreground border-border">
+                  {call.caller.name}
+                </TableCell>
+                <TableCell
+                  className={`text-foreground border-border text-right ${callColor}`}
+                >
+                  -
+                </TableCell>
+                <TableCell
+                  className={`text-foreground border-border text-right ${callColor}`}
+                >
+                  <FDV value={call.startFDV} light={true} />
+                </TableCell>
+                <TableCell
+                  className={`text-foreground border-border text-right ${callColor}`}
+                >
+                  <FDV value={call.highestFDV} light={true} />
+                </TableCell>
+                <TableCell
+                  className={`text-foreground border-border text-right ${callColor}`}
+                >
+                  <CallMultiple multiple={call.multiple} />
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
     </>
