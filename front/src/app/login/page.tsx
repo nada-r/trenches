4820064cbd02368a3230ca6f88afe42f3ac67a51
nico <nicolascoquelet@gmail.com>
2024-10-02 14:24:51 +0200

    telegram login wip

--- front/src/app/login/page.tsx
+++ front/src/app/login/page.tsx
@@ -5,20 +5,17 @@ import { useSearchParams } from 'next/navigation';
 
 function LoginInfo() {
   const searchParams = useSearchParams();
+
   return (
     <div>
       <h2>Query Parameters:</h2>
-      {Object.entries(searchParams).length > 0 ? (
-        <ul>
-          {Object.entries(searchParams).map(([key, value]) => (
-            <li key={key}>
-              <strong>{key}:</strong> {value}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No query parameters found.</p>
-      )}
+      <ul>
+        {Array.from(searchParams.entries()).map(([key, value]) => (
+          <li key={key}>
+            {key}: {value}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
