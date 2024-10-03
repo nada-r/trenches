d9499e876a75d0c0e80c77ca817a1cd645ef897d
nada-r <nadarzer@gmail.com>
2024-10-03 15:51:26 +0200

    test files, DO NOT PUSH TO PROD

---
+++ backend/src/index_test.ts
@@ -0,0 +1,89 @@
+import './pre-start'; // Must be the first import
+import logger from 'jet-logger';
+
+import EnvVars from '@src/common/EnvVars';
+import server from './server';
+import {
+  CallerService,
+  CallingPowerService,
+  CallService,
+  ProfileService,
+  TournamentService,
+} from './services';
+import bootstrap from '@src/bootstrap';
+
+interface IServices {
+  caller: CallerService;
+  call: CallService;
+  callingPower: CallingPowerService;
+  tournament: TournamentService;
+  profile: ProfileService;
+}
+
+export const services: Partial<IServices> = {};
+
+/**
+ * Adds a new service to the services object if it matches the interface.
+ * @param name - The name of the service. This is used to index the service in the services object.
+ * @param newService - The constructor function for the service.
+ * @param dependencies - The dependencies for the service.
+ *
+ * @example
+ * // Adding a TaskScheduler service
+ * addService('scheduler', TaskScheduler);
+ *
+ * // Adding a chatService with a PrismaClient dependency
+ * const prisma = new PrismaClient();
+ * addService('chat', chatService, prisma);
+ *
+ * // Using the added services
+ * services.scheduler?.addTask({ running: false, callback: () => console.log("Task executed") });
+ * services.chat?.getMessages(1).then(messages => console.log(messages));
+ */
+export function addService<T extends keyof IServices>(
+  name: T,
+  newService: new (...args: any[]) => IServices[T],
+  ...dependencies: any[]
+): void {
+  if (dependencies.length) services[name] = new newService(...dependencies);
+  else services[name] = new newService();
+}
+
+export function addExistingService<T extends keyof IServices>(
+  name: T,
+  service: IServices[T]
+): void {
+  services[name] = service;
+}
+// **** Run **** //
+
+const SERVER_START_MSG =
+  'Express server started on port: ' + EnvVars.Port.toString();
+
+// example how use service
+// server.get('card/create', async() => {
+//   await services.card?.createCard()
+// });
+
+server.listen(EnvVars.Port, async () => {
+  const {
+    callerService,
+    callService,
+    callingPowerService,
+    profileService,
+    tournamentService,
+  } = await bootstrap();
+  addExistingService('caller', callerService);
+  addExistingService('call', callService);
+  addExistingService('callingPower', callingPowerService);
+  addExistingService('tournament', tournamentService);
+  addExistingService('profile', profileService);
+  const testAddCaller = await callerService.getOrCreateCaller(
+    `${Math.floor(Math.random() * 1000000)}`,
+   `RandomUser${Math.floor(Math.random() * 10000)}`,
+   "https://trenches.fra1.cdn.digitaloceanspaces.com/Shrek.jpg");
+     console.log("🚀 ~ server.listen ~ testAddCaller:", testAddCaller)
+
+
+  logger.info(SERVER_START_MSG);
+});
