1c4d7c7a45efa02c6248cbd494599136af720ff8
nada-r <nadarzer@gmail.com>
2024-10-13 01:23:28 +0200

    rm 2 pages from footer

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -1,44 +1,44 @@
-import React from 'react';
-import Link from 'next/link';
-import RankIcon from './icons/RankIcon';
-import PortfolioIcon from './icons/PortfolioIcon';
-import MarketIcon from './icons/MarketIcon';
-import TournamentIcon from './icons/TournamentIcon';
-import CallIcon from '@/components/icons/CallIcon';
+import React from "react";
+import Link from "next/link";
+import RankIcon from "./icons/RankIcon";
+import PortfolioIcon from "./icons/PortfolioIcon";
+import MarketIcon from "./icons/MarketIcon";
+import TournamentIcon from "./icons/TournamentIcon";
+import CallIcon from "@/components/icons/CallIcon";
 
 const menulist = [
   {
     icon: <RankIcon />,
-    name: 'Ranking',
-    href: '/ranking',
+    name: "Ranking",
+    href: "/ranking",
   },
   {
     icon: <PortfolioIcon />,
-    name: 'Portfolio',
-    href: '/portfolio',
+    name: "Portfolio",
+    href: "/portfolio",
   },
   {
     icon: <CallIcon />,
-    name: 'Calls',
-    href: '/call',
+    name: "Calls",
+    href: "/call",
   },
-  {
-    icon: <MarketIcon />,
-    name: 'Market',
-    href: '/market',
-  },
-  {
-    icon: <TournamentIcon />,
-    name: 'Tournament',
-    href: '/tournament',
-  },
+  // {
+  //   icon: <MarketIcon />,
+  //   name: "Market",
+  //   href: "/market",
+  // },
+  // {
+  //   icon: <TournamentIcon />,
+  //   name: "Tournament",
+  //   href: "/tournament",
+  // },
 ];
 
 const Footer: React.FC = () => {
   return (
-    <footer className="sticky z-[999] bottom-0 w-full flex justify-around bg-background text-foreground border-t border-border py-2">
+    <footer className="sticky z-[999] bottom-0 w-full flex justify-center bg-background text-foreground border-t border-border py-2">
       {menulist.map((menu) => (
-        <Link href={menu.href} className="text-foreground no-underline">
+        <Link href={menu.href} className="text-foreground no-underline mx-4">
           <div className="flex flex-col items-center">
             <div className="shrink-0">{menu.icon}</div>
             <p className="m-0 text-xs">{menu.name}</p>
